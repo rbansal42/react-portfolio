59c6b00a69073b9ecadbc8b8d523ad0f5447f99d
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-12 16:05:00 +0530

    Added scss for App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
 import { Navbar } from "./components";
+import "./App.scss";
 
 const App = () => {
 	return (
 		<div className="app">
-      <Navbar />
-      <Header />
-      <About />
-      <Skills />
-      <Work />
-      <Testimonials />
-      <Footer />
+			<Navbar />
+			<Header />
+			<About />
+			<Skills />
+			<Work />
+			<Testimonials />
+			<Footer />
 		</div>
 	);
 };
