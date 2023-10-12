1f9441771024bc957664753bdba7fbd54a485dd7
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-12 15:56:21 +0530

    Structure of website - Imported all needed containers and component - Laid out imports in structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
-	return <div><h1>App</h1></div>;
+	return (
+		<div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Work />
+      <Testimonials />
+      <Footer />
+		</div>
+	);
 };
 
 export default App;
